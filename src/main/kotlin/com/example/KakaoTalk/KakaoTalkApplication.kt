51999469ec676950605d5b6cee8d01e668d86f24
essential2189 <70889358+essential2189@users.noreply.github.com>
2023-07-26 21:37:50 +0900

    project init

---
+++ src/main/kotlin/com/example/KakaoTalk/KakaoTalkApplication.kt
@@ -0,0 +1,11 @@
+package com.example.KakaoTalk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KakaoTalkApplication
+
+fun main(args: Array<String>) {
+	runApplication<KakaoTalkApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/KakaoTalk/KakaoTalkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.KakaoTalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KakaoTalkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
