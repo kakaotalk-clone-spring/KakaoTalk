2cb298c437b9d0b874db22bc5583a65757dd521d
ysndy <ysndy@naver.com>
2023-08-11 20:09:53 +0900

    fix:#6 BaseException 으로 기본 예외 클래스 변경

--- src/main/kotlin/com/example/KakaoTalk/common/exception/CustomException.kt
+++
@@ -1,6 +0,0 @@
-package com.example.KakaoTalk.common.exception
-
-import com.example.KakaoTalk.common.response.ResponseStatus
-
-class CustomException(val errorCode: ResponseStatus) : RuntimeException() {
-}
--- src/main/kotlin/com/example/KakaoTalk/member/service/MemberService.kt
+++ src/main/kotlin/com/example/KakaoTalk/member/service/MemberService.kt
@@ -1,6 +1,6 @@
 package com.example.KakaoTalk.member.service
 
-import com.example.KakaoTalk.common.exception.CustomException
+import com.example.KakaoTalk.common.exception.BaseException
 import com.example.KakaoTalk.common.util.LOGIN_USER
 import com.example.KakaoTalk.member.repository.MemberRepository
 import com.example.KakaoTalk.member.domain.Member
@@ -17,10 +17,10 @@ class MemberService (private val memberRepo: MemberRepository, private val passw
     fun login(session: HttpSession, loginDto: LoginDto) : String {
         //회원 정보 가져오기
         val member = memberRepo.findById(loginDto.id)
-            .orElseThrow { CustomException(LoginResponseStatus.AUTHENTICATION_ERROR) }
+            .orElseThrow { BaseException(LoginResponseStatus.AUTHENTICATION_ERROR) }
         //비밀번호 검증
         if(!member.isPasswordMatch(passwordEncoder, loginDto.password)) {
-            throw CustomException(LoginResponseStatus.ACCESS_ERROR)
+            throw BaseException(LoginResponseStatus.ACCESS_ERROR)
         }
         session.setAttribute(LOGIN_USER, member)
         return session.id
