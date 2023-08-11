e352ea368684138dc426528752763a2e4b1a7c87
ysndy <ysndy@naver.com>
2023-08-11 20:07:49 +0900

    fix:#19 BaseException.kt 구현

--- src/main/kotlin/com/example/KakaoTalk/common/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/KakaoTalk/common/handler/GlobalExceptionHandler.kt
@@ -1,18 +1,25 @@
 package com.example.KakaoTalk.common.handler
+import com.example.KakaoTalk.common.exception.BaseException
 import com.example.KakaoTalk.common.response.BaseResponse
 import com.example.KakaoTalk.common.response.BaseResponseStatus
+import com.example.KakaoTalk.common.response.ResponseStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
+    @ExceptionHandler(BaseException::class)
+    private fun handleBaseException(e: BaseException): BaseResponse<Unit>{
+        return handleExceptionInternal(e.responseStatus)
+    }
+
     @ExceptionHandler(Exception::class)
     private fun handleException(e: Exception): BaseResponse<Unit> {
         return handleExceptionInternal(BaseResponseStatus.SERVER_ERROR)
     }
 
-    private fun handleExceptionInternal(status: BaseResponseStatus): BaseResponse<Unit> {
+    private fun handleExceptionInternal(status: ResponseStatus): BaseResponse<Unit> {
         return BaseResponse(status)
     }
 
