7b3dadde2146951fcc83c8de103680027a5fb943
ysndy <ysndy@naver.com>
2023-08-18 02:51:40 +0900

    feat:#4 친구 도메인 구현

---
+++ src/main/kotlin/com/example/KakaoTalk/friends/domain/Friend.kt
@@ -0,0 +1,10 @@
+package com.example.KakaoTalk.friends.domain
+
+data class Friend(
+
+    val id : String,
+    val name : String,
+    val profile_img : String?,
+    val background_img : String?,
+
+)
