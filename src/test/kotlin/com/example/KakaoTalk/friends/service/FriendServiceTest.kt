2d0cbdf4a2f66fc432b28bbf77ad9f667df0dc9b
ysndy <ysndy@naver.com>
2023-08-18 11:57:02 +0900

    test:#4 친구 서비스 테스트 코드 작성

---
+++ src/test/kotlin/com/example/KakaoTalk/friends/service/FriendServiceTest.kt
@@ -0,0 +1,24 @@
+package com.example.KakaoTalk.friends.service
+
+import com.example.KakaoTalk.member.domain.Member
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class FriendServiceTest(
+    private val friendService: FriendService
+) {
+
+    @Test
+    fun 친구_추가() {
+        //given
+        //val member =
+
+        //when
+
+        //then
+    }
+
+
+}
