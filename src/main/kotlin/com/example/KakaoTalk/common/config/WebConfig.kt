0fc5587088c38b1c685f949e51d11249ad5adeab
essential2189 <70889358+essential2189@users.noreply.github.com>
2023-09-03 23:19:33 +0900

    feat: cors 설정

---
+++ src/main/kotlin/com/example/KakaoTalk/common/config/WebConfig.kt
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+            .allowedHeaders("headers")
+            .maxAge(3000)
+    }
+}
